468175b5c2d68ac5a00755f275a52d3ba6380b0e
WilllQ <wquan0772@students.mc3.edu>
2024-04-18 17:38:08 -0400

    KC11.java Addition
    Added KC11 to print "hello world" and then several hellos.

---
+++ KC11.java
@@ -0,0 +1,9 @@
+public class KC11 {
+   public static void main(String[]a) {
+      System.out.println("hello world");
+      
+      for(int x = 1; x <= 100; x++) {
+         System.out.println("hello");
+      }
+   }
+}
